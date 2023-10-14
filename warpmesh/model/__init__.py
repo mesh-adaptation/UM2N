761c2ae46893b8ef099fdf356534fdfe0007bd46
chunyang.w <cw1722@ic.ac.uk>
2023-10-14 22:39:01 +0100

    add M2N_og model

--- warpmesh/model/__init__.py
+++ warpmesh/model/__init__.py
@@ -4,4 +4,5 @@ from .gatdeformer import *  # noqa
 from .train_util import *  # noqa
 from .MRN import *  # noqa
 from .M2N import *  # noqa
+from .M2N_og import * # noqa
 from .extractor import *  # noqa
--- warpmesh/model/extractor.py
+++ warpmesh/model/extractor.py
@@ -33,8 +33,8 @@ class LocalFeatExtractor(MessagePassing):
         super().__init__(aggr='add')
         # 1*distance + 2*feat + 2*coord
         num_in_feat = 1 + (num_feat-2)*2 + 2
-        self.lin_1 = torch.nn.Linear(num_in_feat, 32)
-        self.lin_2 = torch.nn.Linear(32, 64)
+        self.lin_1 = torch.nn.Linear(num_in_feat, 64)
+        self.lin_2 = torch.nn.Linear(64, 64)
         # minus 3 because dist, corrd is added back
         self.lin_3 = torch.nn.Linear(64, out-1)
         self.activate = torch.nn.SELU()
@@ -90,7 +90,7 @@ class GlobalFeatExtractor(torch.nn.Module):
         dropout (torch.nn.Dropout): Dropout layer.
         final_pool (torch.nn.AdaptiveAvgPool2d): Final pooling layer.
     """
-    def __init__(self, in_c, out_c, drop_p=0.2):
+    def __init__(self, in_c, out_c, drop_p=0.2, use_drop=True):
         super().__init__()
         """
         Initialize the layer.
@@ -99,6 +99,10 @@ class GlobalFeatExtractor(torch.nn.Module):
             in_c (int): Number of input channels.
             out_c (int): Number of output channels.
             drop_p (float, optional): Dropout probability.
+            use_drop: (bool, optional): Use dropout layer or not,
+                When it is set to `False`, this building block is exactly
+                the block used in the original M2N model with out any
+                change. Then set to `True`, it is used for MRN model.
         """
         self.in_c = in_c
         self.out_c = out_c
@@ -107,6 +111,7 @@ class GlobalFeatExtractor(torch.nn.Module):
         self.conv3 = torch.nn.Conv2d(64, 32, 3, padding=2, stride=1)
         self.conv4 = torch.nn.Conv2d(32, out_c, 3, padding=2, stride=1)
         self.dropout = torch.nn.Dropout(drop_p)
+        self.use_drop = use_drop
         self.final_pool = torch.nn.AdaptiveAvgPool2d(1)
 
     def forward(self, data):
@@ -121,16 +126,16 @@ class GlobalFeatExtractor(torch.nn.Module):
         """
         x = self.conv1(data)
         x = F.selu(x)
-        x = self.dropout(x)  # apply dropout after activation
+        x = self.dropout(x) if self.use_drop else x
         x = self.conv2(x)
         x = F.selu(x)
-        x = self.dropout(x)  # apply dropout after activation
+        x = self.dropout(x) if self.use_drop else x
         x = self.conv3(x)
         x = F.selu(x)
-        x = self.dropout(x)  # apply dropout after activation
+        x = self.dropout(x) if self.use_drop else x
         x = self.conv4(x)
         x = F.selu(x)
-        x = self.dropout(x)  # apply dropout after activation
+        x = self.dropout(x) if self.use_drop else x
         x = self.final_pool(x)
         x = x.reshape(-1, self.out_c)
         return x
