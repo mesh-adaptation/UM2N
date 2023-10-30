11490d07df7a52c02142d4b3511dce3794af0ca0
chunyang.w <cw1722@ic.ac.uk>
2023-10-30 23:48:55 +0000

    fix again and again, i am stupid

--- warpmesh/model/train_util.py
+++ warpmesh/model/train_util.py
@@ -235,7 +235,7 @@ def train(
         optimizer.step()
         total_loss += loss.item()
         total_deform_loss += deform_loss.item() 
-        total_inversion_loss += inversion_loss.item() if not use_inversion_loss else 0 # noqa
+        total_inversion_loss += inversion_loss.item() if use_inversion_loss else 0 # noqa
     if (use_inversion_loss):
         return {
             "total_loss": total_loss / len(loader),
