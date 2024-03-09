8890c089f873d517851e6c2389bf2d02dc2469c2
mingrui <mingrui.zhang@taichi.graphics>
2024-03-09 18:59:01 +0800

    update combine vis

--- combine_vis.py
+++ combine_vis.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import glob
+import yaml
 import matplotlib.pyplot as plt
 import firedrake as fd
 
@@ -29,12 +30,28 @@ trained_epoch = 999
 problem_type = "helmholtz_square"
 dataset_path = "./data/dataset_meshtype_6/helmholtz/z=<0,1>_ndist=None_max_dist=6_lc=0.05_n=100_aniso_full_meshtype_6"
 dataset_name = dataset_path.split("/")[-1]
+result_folder = f"./compare_output/{dataset_name}"
+os.makedirs(result_folder, exist_ok=True)
 
-num_vis = 1
+
+info_dict = {}
+info_dict["run_ids"] = run_ids
+info_dict["names"] = run_id_model_mapping
+info_dict["epoch"] = trained_epoch
+info_dict["problem_type"] = problem_type
+info_dict["dataset_name"] = dataset_name
+info_dict["dataset_path"] = dataset_path
+# Write the dictionary to a YAML file
+with open(f"{result_folder}/models_info" + ".yaml", "w") as file:
+    yaml.dump(info_dict, file, default_flow_style=False)
+
+num_vis = 5
 rows = 3
 cols = 3 + len(run_ids)
 for n_v in range(num_vis):
+    print(f"=== Visualizing number {n_v} of {dataset_name} ===")
     # Load mesh for visualization
+    mesh_og = fd.Mesh(os.path.join(dataset_path, "mesh", f"mesh_{n_v:04d}.msh"))
     mesh_MA = fd.Mesh(os.path.join(dataset_path, "mesh", f"mesh_{n_v:04d}.msh"))
     mesh_fine = fd.Mesh(os.path.join(dataset_path, "mesh_fine", f"mesh_{n_v:04d}.msh"))
     mesh_model = fd.Mesh(os.path.join(dataset_path, "mesh", f"mesh_{n_v:04d}.msh"))
@@ -60,7 +77,8 @@ for n_v in range(num_vis):
 
     plot_data_dicts = {}
     for model_name, run_id in zip(model_names, run_ids):
-        print(f"model name: {model_name}, run id: {run_id}")
+        show_name = run_id_model_mapping[run_id]
+        print(f"model name: {show_name}, run id: {run_id}")
         eval_ret_path = f"./eval/{model_name}_{trained_epoch}_{run_id}/{problem_type}/{dataset_name}"
         # print(eval_ret_path)
         eval_exp_path = sorted(glob.glob(f"{eval_ret_path}/*"))[-1]
@@ -171,8 +189,15 @@ for n_v in range(num_vis):
     plot_data_dict = plot_data_dicts[run_ids[0]]
 
     mesh_ma_data = plot_data_dict["mesh_ma"]
+    mesh_MA.coordinates.dat.data[:] = mesh_ma_data
     monitor_values_data = plot_data_dict["monitor_values"]
     monitor_values.dat.data[:] = monitor_values_data
+
+    err_map_orignal_data = plot_data_dict["error_map_original"]
+    error_map_original.dat.data[:] = err_map_orignal_data
+
+    err_map_ma_data = plot_data_dict["error_map_ma"]
+    error_map_ma.dat.data[:] = err_map_ma_data
 
     u_exact_data = plot_data_dict["u_exact"]
     u_exact.dat.data[:] = u_exact_data
@@ -186,6 +211,12 @@ for n_v in range(num_vis):
     # High resolution mesh
     fd.triplot(mesh_fine, axes=ax[0, 0])
     ax[0, 0].set_title(f"High resolution Mesh (100 x 100)")
+    # Orginal low resolution uniform mesh
+    fd.triplot(mesh_og, axes=ax[0, 1])
+    ax[0, 1].set_title(f"Original uniform Mesh")
+    # Adapted mesh (MA)
+    fd.triplot(mesh_MA, axes=ax[0, 2])
+    ax[0, 2].set_title(f"Adapted Mesh (MA)")
 
     # Solution on high resolution mesh
     cb = fd.tripcolor(
@@ -211,9 +242,31 @@ for n_v in range(num_vis):
     ax[2, 0].set_title(f"Monitor values")
     plt.colorbar(cb)
 
+    # Error on orginal low resolution uniform mesh
+    cb = fd.tripcolor(
+        error_map_original,
+        cmap=cmap,
+        axes=ax[2, 1],
+        vmax=error_v_max,
+        vmin=-error_v_max,
+    )
+    ax[2, 1].set_title(f"Error (u-u_exact) uniform Mesh | L2 Norm: {error_og_mesh:.5f}")
+    plt.colorbar(cb)
+    # Error on adapted mesh (MA)
+    cb = fd.tripcolor(
+        error_map_ma,
+        cmap=cmap,
+        axes=ax[2, 2],
+        vmax=error_v_max,
+        vmin=-error_v_max,
+    )
+    ax[2, 2].set_title(
+        f"Error (u-u_exact) MA| L2 Norm: {error_ma_mesh:.5f} | {(error_og_mesh-error_ma_mesh)/error_og_mesh*100:.2f}%"
+    )
+    plt.colorbar(cb)
+
     for rr in range(rows):
         for cc in range(cols):
             ax[rr, cc].set_aspect("equal", "box")
 
-    print(ax)
-    fig.savefig("compare_output/test_load_back.png")
+    fig.savefig(f"{result_folder}/compare_ret_{n_v:04d}.png")
