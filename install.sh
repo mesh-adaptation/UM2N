40f03fb51491056325f02a80a73b9194a60b0b32
Joe Wallwork <joe.wallwork@outlook.com>
2024-09-18 07:39:07 +0100

    Install script tweaks

--- install.sh
+++ install.sh
@@ -16,24 +16,24 @@ function green_log() {
 
 yellow_log "Please deactivate conda enviroment before running this script, otherwise it will fail."
 
-INSTALL_DIR=$(pwd)
+UM2N_ROOT=$(pwd)
+INSTALL_DIR=${UM2N_ROOT}/install
 
-# Download & install firedrake
-cd ./install
+# Download and install Firedrake
+mkdir ${INSTALL_DIR}
+cd ${INSTALL_DIR}
 
-# If previous file exists
-if [ -d ./firedrake ]; then
-  green_log "The directory has existed, nothing to be done"
-fi
-
-if [ ! -d ./firedrake ]; then
+# Build Firedrake if it does not already exist
+if [ -d ${INSTALL_DIR}/firedrake ]; then
+  green_log "A Firedrake installation already exists, nothing to be done"
+else
   curl -O https://raw.githubusercontent.com/firedrakeproject/firedrake/master/scripts/firedrake-install
   python3 firedrake-install
   green_log "Firedrake has been installed"
 fi
 
 # Activate Firedrake enviroment
-. ./firedrake/bin/activate
+. ${INSTALL_DIR}/firedrake/bin/activate
 
 # Install PyTorch
 green_log "Downloading PyTorch..."
@@ -45,11 +45,8 @@ python3 -m pip install "git+https://github.com/facebookresearch/pytorch3d"
 
 # Install Movement
 green_log "Downloading Movement..."
-cd ${VIRTUAL_ENV}/src
-git clone https://github.com/mesh-adaptation/movement.git
-cd movement
-pip install -e .
+git clone https://github.com/mesh-adaptation/movement.git ${VIRTUAL_ENV}/src/movement
+pip install -e ${VIRTUAL_ENV}/src/movement
 
 # Install WarpMesh
-cd ${INSTALL_DIR}
-pip install -e .
+pip install -e ${UM2N_ROOT}
