8a8106141906c5916ea30c4cda65ef8004a7cfd2
Joe Wallwork <jw2423@cam.ac.uk>
2024-11-02 18:23:29 +0000

    Fix imports

--- UM2N/test/__init__.py
+++ UM2N/test/__init__.py
@@ -1,6 +1,5 @@
 from .tangle import *  # noqa
 from .compare_hlmhltz import *  # noqa
-from .bench_helmholtz import *  # noqa
 from .bench_swirl import *  # noqa
 from .bench_burgers import *  # noqa
 from .stat import *  # noqa
