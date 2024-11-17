89480dbfb494706aa83093cbab719a7f0e9393f4
Davor Dundovic <33790330+ddundo@users.noreply.github.com>
2024-11-17 22:26:23 +0100

    Fix test_import.py
    Closes #57

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,31 +1,11 @@
-# Author: Chunyang Wang
-# GitHub Username: acse-cw1722
-
-from pytest import fixture
-
-
-@fixture(scope="module")
-def UM2N():
+def test_import_um2n():
     import UM2N
-
-    return UM2N
-
-
-@fixture(scope="module")
-def firedrake():
-    import firedrake
-
-    return firedrake
-
-
-@fixture(scope="module")
-def movement():
-    import movement
-
-    return movement
-
-
-def test_import(UM2N, firedrake, movement):
     assert UM2N
+
+def test_import_firedrake():
+    import firedrake
     assert firedrake
+
+def test_import_movement():
+    import movement
     assert movement
