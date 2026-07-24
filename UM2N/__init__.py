9860fbdef46ec23fc39c7232839834567fb4408a
Stephan Kramer <s.kramer@imperial.ac.uk>
2026-07-24 12:03:24 +0100

    Remove dependency on pkg_resources
    This is a deprecated module of setuptools. We can live without a
    __version__ string for now.

--- UM2N/__init__.py
+++ UM2N/__init__.py
@@ -10,8 +10,6 @@ except ImportError:
 
 os.environ["OMP_NUM_THREADS"] = "1"
 
-from pkg_resources import DistributionNotFound, get_distribution  # noqa
-
 from .processor import *  # noqa
 from .generator import *  # noqa
 from .model import *  # noqa
@@ -19,8 +17,3 @@ from .loader import *  # noqa
 from .helper import *  # noqa
 from .test import *  # noqa
 
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    pass
